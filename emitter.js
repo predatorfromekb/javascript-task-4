c489710a7aede7321412a89d1b23627e901df7a4
predatorfromekb <stepanus97@yandex.ru>
2018-11-05 03:49:42 +0500

    решение

--- emitter.js
+++ emitter.js
@@ -11,6 +11,41 @@ const isStar = true;
  * @returns {Object}
  */
 function getEmitter() {
+    const events = new Map();
+
+    function subscribe(namespace, context, handler, executeFunc = () => true) {
+        if (!events.get(namespace)) {
+            events.set(namespace, []);
+        }
+        events.get(namespace).push({
+            handler: handler,
+            context: context,
+            executeFunc: executeFunc,
+            counter: 0
+        });
+    }
+
+    function unsubscribe(namespace, context) {
+        const objects = events.get(namespace);
+        if (!objects) {
+            return;
+        }
+        events.set(namespace, objects.filter(e => e.context !== context));
+    }
+
+    function apply(namespace) {
+        const objects = events.get(namespace);
+        if (!objects) {
+            return;
+        }
+        objects.forEach(object => {
+            if (object.executeFunc()) {
+                object.handler.call(object.context);
+            }
+            object.counter++;
+        });
+    }
+
     return {
 
         /**
@@ -18,26 +53,61 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object} this
          */
         on: function (event, context, handler) {
+            if (!event && !context && !handler) {
+                return this;
+            }
+
+            subscribe(event, context, handler);
             console.info(event, context, handler);
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object} this
          */
         off: function (event, context) {
+            if (!event && !context) {
+                return this;
+            }
+            unsubscribe(event, context);
+            events.forEach((array, namespace) => {
+                if (namespace.indexOf(`${event}.`) === 0) {
+                    unsubscribe(namespace, context);
+                }
+            });
             console.info(event, context);
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object} this
          */
         emit: function (event) {
-            console.info(event);
+            if (!event) {
+                return this;
+            }
+            const parts = event.split('.');
+            const namespaces = [];
+            parts.forEach((value, index) => {
+                namespaces.push(index === 0
+                    ? value
+                    : `${namespaces[index - 1]}.${value}`);
+            });
+            namespaces.reverse();
+
+            namespaces.forEach(value => apply(value));
+
+            return this;
         },
 
         /**
@@ -47,9 +117,22 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object} this
          */
         several: function (event, context, handler, times) {
+            if (!event && !context && !handler) {
+                return this;
+            }
+            if (times <= 0) {
+                return this.on(event, context, handler);
+            }
+
+            subscribe(event, context, handler, function () {
+                return this.counter < times;
+            });
             console.info(event, context, handler, times);
+
+            return this;
         },
 
         /**
@@ -59,9 +142,22 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object} this
          */
         through: function (event, context, handler, frequency) {
+            if (!event && !context && !handler) {
+                return this;
+            }
+            if (frequency <= 0) {
+                return this.on(event, context, handler);
+            }
+
+            subscribe(event, context, handler, function () {
+                return this.counter % frequency === 0;
+            });
             console.info(event, context, handler, frequency);
+
+            return this;
         }
     };
 }
